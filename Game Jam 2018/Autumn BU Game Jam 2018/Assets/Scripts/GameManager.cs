e6f9dee5c77be45fe33de69a15b4078eefba9aeb
MechSam <samuelbrown3199@gmail.com>
2018-11-08 20:13:47 +0000

    Controller and Local multiplayer

---
+++ Game Jam 2018/Autumn BU Game Jam 2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int numOfPlayers = 1;
+    public List<TankManager> tanks;
+
+    public GameObject tank;
+
+    private void Start()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        for(int i = 0; i < numOfPlayers; i++)
+        {
+            GameObject curTank = Instantiate(tank, transform.position, Quaternion.identity);
+            tanks.Add(curTank.GetComponent<TankManager>());
+            tanks[i].playerNumber = i + 1;
+        }
+    }
+}
--- Game Jam 2018/Autumn BU Game Jam 2018/Assets/Scripts/TankManager.cs
+++ Game Jam 2018/Autumn BU Game Jam 2018/Assets/Scripts/TankManager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class TankManager : MonoBehaviour
 {
+    public int playerNumber;
 
-    public Rigidbody rb;
+    public Rigidbody rb, turretRb;
     public float movementForce;
 
 	void Start ()
@@ -20,30 +21,90 @@ public class TankManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        MovementManager();    
+        InputManager();
     }
 
-    void MovementManager()
+    void InputManager()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (playerNumber == 1)
         {
-            rb.AddForce(transform.forward * movementForce);
+            if (Input.GetKey(KeyCode.W) || Input.GetAxis("Vertical_P1") > 0.1f)
+            {
+                rb.AddForce(transform.forward * movementForce);
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetAxis("Vertical_P1") < -0.1f)
+            {
+                rb.AddForce(-transform.forward * (movementForce / 2f));
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetAxis("Horizontal_P1") < -0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, -50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetAxis("Horizontal_P1") > 0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, 50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+
+            if (Input.GetKey(KeyCode.E) || Input.GetAxis("TurretRotation_P1") > 0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, 50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                turretRb.MoveRotation(turretRb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.Q) || Input.GetAxis("TurretRotation_P1") < -0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, -50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                turretRb.MoveRotation(turretRb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.F) || Input.GetButton("Fire1_P1"))
+            {
+                Debug.Log("FIRE PLAYER 1");
+            }
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (playerNumber == 2)
         {
-            rb.AddForce(-transform.forward * (movementForce / 2f));
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Vector3 m_EulerAngleVelocity = new Vector3(0, -50, 0);
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Vector3 m_EulerAngleVelocity = new Vector3(0, 50, 0);
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
+            if (Input.GetKey(KeyCode.U) || Input.GetAxis("Vertical_P2") > 0.1f)
+            {
+                rb.AddForce(transform.forward * movementForce);
+            }
+            if (Input.GetKey(KeyCode.J) || Input.GetAxis("Vertical_P2") < -0.1f)
+            {
+                rb.AddForce(-transform.forward * (movementForce / 2f));
+            }
+            if (Input.GetKey(KeyCode.H) || Input.GetAxis("Horizontal_P2") < -0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, -50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.K) || Input.GetAxis("Horizontal_P2") > 0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, 50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+
+            if (Input.GetKey(KeyCode.I) || Input.GetAxis("TurretRotation_P2") > 0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, 50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                turretRb.MoveRotation(turretRb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.Y) || Input.GetAxis("TurretRotation_P2") < -0.1f)
+            {
+                Vector3 m_EulerAngleVelocity = new Vector3(0, -50, 0);
+                Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+                turretRb.MoveRotation(turretRb.rotation * deltaRotation);
+            }
+            if (Input.GetKey(KeyCode.L) || Input.GetButton("Fire1_P2"))
+            {
+                Debug.Log("FIRE PLAYER 2");
+            }
         }
     }
 }
