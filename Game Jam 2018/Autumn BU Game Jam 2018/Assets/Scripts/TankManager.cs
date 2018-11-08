309fb4d92231775cde733f6fb1b94bddfad155e5
MechSam <samuelbrown3199@gmail.com>
2018-11-08 16:19:03 +0000

    Early Tank Code
    This doesnt work, im just switching over to my laptop.

---
+++ Game Jam 2018/Autumn BU Game Jam 2018/Assets/Scripts/TankManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankManager : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        MovementManager();    
+    }
+
+    void MovementManager()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f);
+            transform.localPosition = newPos;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Quaternion newRot = new Quaternion(transform.rotation.x, transform.rotation.y + 0.1f, transform.rotation.z, 0);
+            rb.rotation = newRot;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion newRot = new Quaternion(transform.rotation.x, transform.rotation.y - 0.1f, transform.rotation.z, 0);
+            rb.rotation = newRot;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion newRot = new Quaternion(transform.rotation.x, transform.rotation.y + 0.1f, transform.rotation.z, 0);
+            transform.rotation = newRot;
+        }
+    }
+}
